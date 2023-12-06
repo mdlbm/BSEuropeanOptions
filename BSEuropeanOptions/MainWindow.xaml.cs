c3b55727365958abce095f650ade948c71660ccb
mdlbm <mats.bererd@ensae.fr>
2023-12-06 13:43:26 +0100

    Ajoutez des fichiers projet.

---
+++ BSEuropeanOptions/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+
+namespace OptionPricingApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double stockPrice = Convert.ToDouble(txtStockPrice.Text);
+                double strikePrice = Convert.ToDouble(txtStrikePrice.Text);
+                double interestRate = Convert.ToDouble(txtInterestRate.Text) / 100.0;
+                double dividendYield = Convert.ToDouble(txtDividendYield.Text) / 100.0;
+                double volatility = Convert.ToDouble(txtVolatility.Text) / 100.0;
+                double timeToMaturity = Convert.ToDouble(txtTimeToMaturity.Text);
+
+                double optionPrice = 0.0;
+
+                if (rbCall.IsChecked == true)
+                {
+                    optionPrice = OptionPricing.CalculateCallOptionPrice(
+                        stockPrice, strikePrice, interestRate, dividendYield, volatility, timeToMaturity);
+                }
+                else if (rbPut.IsChecked == true)
+                {
+                    optionPrice = OptionPricing.CalculatePutOptionPrice(
+                        stockPrice, strikePrice, interestRate, dividendYield, volatility, timeToMaturity);
+                }
+
+                txtResult.Text = $"Option Price: {optionPrice:C2}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ BSEuropeanOptions/OptionPricing.cs
@@ -0,0 +1,38 @@
+﻿using MathNet.Numerics.Distributions;
+using System;
+
+namespace OptionPricingApp
+{
+    public static class OptionPricing
+    {
+        public static double CalculateCallOptionPrice(
+            double stockPrice, double strikePrice, double interestRate,
+            double dividendYield, double volatility, double timeToMaturity)
+        {
+            double d1 = (Math.Log(stockPrice / strikePrice) + (interestRate - dividendYield + 0.5 * Math.Pow(volatility, 2)) * timeToMaturity)
+                       / (volatility * Math.Sqrt(timeToMaturity));
+
+            double d2 = d1 - volatility * Math.Sqrt(timeToMaturity);
+
+            double callOptionPrice = stockPrice * Math.Exp(-dividendYield * timeToMaturity) * Normal.CDF(0, 1, d1)
+                                  - strikePrice * Math.Exp(-interestRate * timeToMaturity) * Normal.CDF(0, 1, d2);
+
+            return callOptionPrice;
+        }
+
+        public static double CalculatePutOptionPrice(
+            double stockPrice, double strikePrice, double interestRate,
+            double dividendYield, double volatility, double timeToMaturity)
+        {
+            double d1 = (Math.Log(stockPrice / strikePrice) + (interestRate - dividendYield + 0.5 * Math.Pow(volatility, 2)) * timeToMaturity)
+                       / (volatility * Math.Sqrt(timeToMaturity));
+
+            double d2 = d1 - volatility * Math.Sqrt(timeToMaturity);
+
+            double putOptionPrice = strikePrice * Math.Exp(-interestRate * timeToMaturity) * Normal.CDF(0, 1, -d2)
+                                 - stockPrice * Math.Exp(-dividendYield * timeToMaturity) * Normal.CDF(0, 1, -d1);
+
+            return putOptionPrice;
+        }
+    }
+}
